044387cb7df10253f66f8c2a5d8dce470da6b8ea
vickomaris <64400928+vickomaris@users.noreply.github.com>
2023-08-25 21:01:22 +0700

    menambahkan bootstrap dan merapihkan style responsif

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ const choices = [
 
 const App = () => {
   const [playerChoice, setPlayerChoice] = useState('');
-  const [computerChoice, setComputerChoice] = useState(''); // Menggunakan null sebagai nilai awal
+  const [computerChoice, setComputerChoice] = useState('');
   const [result, setResult] = useState('');
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
   const [drawCount, setDrawCount] = useState(0);
   const [round, setRound] = useState(0);
 
-  const gameResult = () => {
+  const gameSuit = () => {
     if (playerChoice !== '') {
       const computerPick = choices[Math.floor(Math.random() * choices.length)];
       setComputerChoice(computerPick);
@@ -41,59 +41,62 @@ const App = () => {
 
       setRound(round + 1);
     } else {
-      setComputerChoice(null); // Set computerChoice menjadi null jika pemain belum memilih
+      setComputerChoice('');
       setResult('');
     }
   };
 
-  useEffect(gameResult, [playerChoice]);
+  useEffect(gameSuit, [playerChoice]);
 
   const handleChoice = (choice) => {
     setPlayerChoice(choice);
   };
 
   return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Rock Paper Scissors</h1>
+    <div className="container">
+      <h1 className="mb-5 text-center">Rock Paper Scissors</h1>
       <div className="row">
-        <div className="col-md-6">
-          <h3>Player</h3>
-          <div className="choices">
-            {choices.map((choice, index) => (
-              <button
-                key={index}
-                className="btn btn-primary m-1"
-                onClick={() => handleChoice(choice.name)}
-              >
-                <img src={`images/${choice.image}`} alt={choice.name} />
-              </button>
-            ))}
+        <div className="col-md-8">
+          <div className="choices text-center">
+            <div className="sideComputer mb-3">
+              <h3>Computer</h3>
+              {computerChoice ? (
+                <p>
+                  <img
+                    src={`images/${computerChoice.image}`}
+                    alt={computerChoice.name}
+                    className="imgComputer"
+                  />
+                </p>
+              ) : (
+                <p>Waiting for player to choose...</p>
+              )}
+            </div>
+            <div className="sidePlayer">
+              <h3>Player</h3>
+              {choices.map((choice, index) => (
+                <button
+                  key={index}
+                  className="btn btn-outline-light m-1"
+                  onClick={() => handleChoice(choice.name)}
+                >
+                  <img
+                    src={`images/${choice.image}`}
+                    alt={choice.name}
+                    className="imgPlayer"
+                  />
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-        <div className="col-md-6">
-          <h3>Computer</h3>
-          {computerChoice ? (
-            <p>
-              <img
-                src={`images/${computerChoice.image}`}
-                alt={computerChoice.name}
-              />
-            </p>
-          ) : (
-            <p>Waiting for player to choose...</p>
-          )}
-        </div>
-      </div>
-      <div className="row mt-4">
-        <div className="col-md-6">
-          <h4>Result: {result}</h4>
-        </div>
-        <div className="col-md-6">
-          <h4>
+        <div className="col-md-4">
+          <h4 className="text-center mt-3">Round: {round} </h4>
+          <h4 className="text-center mt-3"> {result ? result : 'result'}</h4>
+          <h4 className="mt-3">
             Score: Player {playerScore} - {computerScore} Computer
           </h4>
-          <h4>Draws:{drawCount}</h4>
-          <p>Round: {round} </p>
+          <h4 className="mt-3">Draws: {drawCount}</h4>
         </div>
       </div>
     </div>
